9019b60062c771a384d94bcb1557c2c62fdb9ff5
ForeverSuper <1064464687@qq.com>
2016-05-12 08:57:30 +0800

    1.0

---
+++ F_Servlet3_zhujie/src/main/java/com/Forever/control/DoControl.java
@@ -0,0 +1,105 @@
+package com.Forever.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="DoControl",
+	urlPatterns="/do",
+	initParams={
+		@WebInitParam(name="first",value="first.jsp"),
+		@WebInitParam(name="second",value="second.jsp")
+	})
+public class DoControl extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public DoControl() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doDelete method of the servlet. <br>
+	 *
+	 * This method is called when a HTTP delete request is received.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doDelete(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	/*	String action=request.getParameter("action");
+		RequestDispatcher dispatcher=null;
+		if(action.equals(FIRST)){
+			dispatcher=request.getRequestDispatcher(map.get(FIRST).toString());
+		}else if(action.equals(SECOND)){
+			dispatcher=request.getRequestDispatcher(map.get(SECOND).toString());
+		}
+		dispatcher.forward(request, response);*/
+		System.out.println("SUCCESS");
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// Put your code here
+		System.out.println(config.getInitParameter("first"));
+		System.out.println(config.getInitParameter("second"));
+	}
+}
